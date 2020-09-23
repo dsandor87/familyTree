844c5996f4cd35cc58f42b6df4746bad7c1577d3
github username <github email address>
2020-09-23 16:12:10 +0100

    TDD practice with jest

--- family-tree.test.js
+++ family-tree.test.js
@@ -1,9 +1,44 @@
-const { describe, test } = require('jest')
 const familyTree = require('./familyTree')
 
 describe('Family Tree', function () {
   test('I am in my family tree', function () {
     const me = familyTree[0]
     expect(me.name).toEqual('Sandor')
+  })
+
+  test('My mother is also there', function () {
+    const mother = familyTree[1]
+    expect(mother.name).toEqual('Ilona')
+  })
+
+  test('My mum is in the tree', function () {
+    const me = familyTree[0]
+    const mum = me.parents[0]
+    expect(mum.name).toEqual('Ilona')
+  })
+  test('My father is in the tree', function () {
+    const me = familyTree[0]
+    const father = me.parents[1]
+    expect(father.name).toEqual('Sandor Istvan')
+  })
+  test('My father age ', function () {
+    const me = familyTree[0]
+    const father = me.parents[1]
+    expect(father.age).toEqual(64)
+  })
+  test('My mother age ', function () {
+    const me = familyTree[0]
+    const mother = me.parents[0]
+    expect(mother.age).toEqual(55)
+  })
+  test('My mother driving skill info  ', function () {
+    const me = familyTree[0]
+    const mother = me.parents[0]
+    expect(mother.drive).toEqual(false)
+  })
+  test('My father driving skill info  ', function () {
+    const me = familyTree[0]
+    const father = me.parents[1]
+    expect(father.drive).toEqual(true)
   })
 })
--- familyTree.js
+++ familyTree.js
@@ -1,3 +1,5 @@
-const me = { name: 'Sandor' }
+const mother = { name: 'Ilona', age: 55, drive: false }
+const father = { name: 'Sandor Istvan', age: 64, drive: true }
+const me = { name: 'Sandor', parents: [mother, father] }
 
-module.exports = [me]
+module.exports = [me, mother, father]
